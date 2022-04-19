0ab2c8e9b9d0e6afa731b5fc1825c6939d7c1a4f
egorsv1985 <egorvs2009@gmail.com>
2022-04-19 16:27:57 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -78,8 +78,8 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 
 // Построение сценариев выполненных задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
-const build = gulp.series(reset, mainTasks);
-const deployZIP = gulp.series(reset, mainTasks, zip);
+const build = gulp.series(reset, mainTasks, purgeCss);
+const deployZIP = gulp.series(reset, mainTasks, purgeCss, zip);
 const svgSprite = svgSprive;
 const purgecss = purgeCss;
 // const deployFTP = gulp.series(reset, mainTasks, ftp);
@@ -97,9 +97,7 @@ export {
 export {
 	svgSprite
 }
-export {
-	purgecss
-}
+
 // export {
 // 	deployFTP
 // }
